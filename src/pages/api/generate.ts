078f6d420fa54599bbee58a738c93d29b5117a03
Piotr Szcześniak <piotr.szcz3sniak@gmail.com>
2023-09-15 17:41:11 +0200

    start adding db connection

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -1,12 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+const sqlite3 = require('sqlite3').verbose();
+let sql;
+
+const db = new sqlite3.Database('./chat-gpt.db', sqlite3.OPEN_READWRITE, (error: any) => {
+  if (error) return console.log(error.message);
+});
+
+// https://www.youtube.com/watch?v=ZRYn6tgnEgM
 
 type ResponseData = {};
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log('running...');
   const body = {
     model: 'gpt-3.5-turbo',
@@ -27,10 +32,7 @@ export default async function handler(
 
   try {
     // createChatCompletion
-    const response = await fetch(
-      `https://api.openai.com/v1/chat/completions`,
-      requestOptions
-    );
+    const response = await fetch(`https://api.openai.com/v1/chat/completions`, requestOptions);
 
     const data = await response.json();
 
